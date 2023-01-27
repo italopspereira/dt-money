f344474275ce769ae81d44eef7002bee22fb92c8
Ítalo Pereira <italo.pspereira@gmail.com>
2023-01-27 16:46:53 -0300

    styled income and outcome button in modal transition

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -98,4 +98,21 @@ export const TransactionTypeButton = styled(
         ? props.theme['green-300']
         : props.theme['red-300']};
   }
+
+  &[data-state='checked'] {
+    color: ${(props) => props.theme.white};
+    background: ${(props) =>
+      props.variant === 'income'
+        ? props.theme['green-500']
+        : props.theme['red-500']};
+
+    svg {
+      color: ${(props) => props.theme.white};
+    }
+  }
+
+  &[data-state='unchecked']:hover {
+    background: ${(props) => props.theme['gray-600']};
+    transition: background-color 0.2s;
+  }
 `
